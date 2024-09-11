20f8766a0192c1fb2662307b10d3ae1351715e2a
Dario-Gago <gagodario1@gmail.com>
2024-09-11 14:25:29 -0300

    Agregando todo

---
+++ app.js
@@ -0,0 +1,6 @@
+const nombres = ['Susana','Esteban','Carla'];
+
+function capturaElemento(valor, indice, array){
+  console.log(` Indice: ${indice}, Valor: ${valor}`)
+}
+nombres.forEach(capturaElemento)
